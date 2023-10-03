6456d864714477ce2e07b1ffd8fad347c4732a1c
José Alisson <55746756+JoseAPordeus@users.noreply.github.com>
2023-10-03 11:28:05 -0300

    Adicionando funções de pular e mover o pássaro

--- FlappyBird.py
+++ FlappyBird.py
@@ -25,6 +25,7 @@ class Passaro:
     VELOCIDADE_ROTACAO = 20
     TEMPO_ANIMACAO = 5
 
+    # inicialização do pássaro
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -34,6 +35,32 @@ class Passaro:
         self.tempo = 0
         self.contagem_imagem = 0
         self.imagem = IMGS[0]
+    
+    def pular(self):
+        self.velocidade = -10.5
+        self.tempo = 0
+        self.altura = self.y
+
+    def mover(self):
+        # calcular o deslocamento
+        self.tempo += 1
+        deslocamento = 1.5 * (self.tempo**2) + self.velocidade * self.tempo
+        
+        # restringir o deslocamento
+        if deslocamento > 16:
+            deslocamento = 16
+        elif deslocamento < 0:
+            deslocamento -= 2
+
+        self.y += deslocamento
+            
+        # o angulo do passaro
+        if deslocamento < 0 or self.y < (self.altura + 50):
+            if self.angulo < self.ROTACAO_MAXIMA:
+                self.angulo = self.ROTACAO_MAXIMA
+        else:
+            if self.angulo > -90:
+                self.angulo -= self.VELOCIDADE_ROTACAO 
 
 
 class Cano:
