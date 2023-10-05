ec84e5ea5501fc64a742dd9b8dc2fe190f1eb35c
José Alisson <55746756+JoseAPordeus@users.noreply.github.com>
2023-10-05 13:05:41 -0300

    Finalizando a função principal movimentação dos objetos

--- FlappyBird.py
+++ FlappyBird.py
@@ -185,6 +185,7 @@ def main():
     while rodando:
         relogio.tick(30)
 
+        # interação com o usuário
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
                 rodando = False
@@ -194,6 +195,35 @@ def main():
                 if evento.key == pygame.K_SPACE:
                     for passaro in passaros:
                         passaro.pular()
+
+        # mover as coisas
+        for passaro in passaros:
+            passaro.mover()
+        chao.mover()
+
+        adicionar_cano = False
+        remover_canos = []
+        for cano in canos:
+            for i, passaro in enumerate(passaros):
+                if cano.colidir(passaro):
+                    passaros.pop(i)
+                if not cano.passou and passaro.x > cano.x:
+                    cano.passou = True
+                    adicionar_cano = True
+            cano.mover()
+            if cano.x + cano.CANO_TOPO.get_width() < 0:
+                remover_canos.appender(cano)
+
+        if adicionar_cano:
+            pontos += 1
+            canos.append(Cano(600))
+        for cano in remover_canos:
+            canos.remove(cano)
+
+        for i, passaro in enumerate(passaros):
+            if (passaro.y + passaro.imagem.get_width()) > chao.y or passaro.y < 0:
+                passaros.pop(i)
+        
 
         desenhar_tela(tela, passaros, canos, chao, pontos)
         
